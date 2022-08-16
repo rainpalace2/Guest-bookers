ea4ec5f749840f693250fc7513c8755160d6a285
rainpalace2 <110389642+rainpalace2@users.noreply.github.com>
2022-08-16 12:32:27 +0000

    [Add]Sign upページ,Log inページ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,12 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books'
   end
 
   def index
@@ -7,4 +14,11 @@ class BooksController < ApplicationController
 
   def show
   end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   
-  
   devise_for :users
-  root to:'homes#top'
+  root to: 'homes#top'
   get "about" => "homes#about", as: "about"
   
-  resources :books,only: [:new,:index,:show]
+  resources :books,only: [:new,:index,:show,:create]
   resources :users,only: [:show,:edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users,controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
 end
