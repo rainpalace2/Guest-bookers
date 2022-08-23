59760711cf1d6504f72f3fa8227d1b6dfc03fc92
rainpalace2 <110389642+rainpalace2@users.noreply.github.com>
2022-08-23 08:54:50 +0000

    [Fix]フラッシュメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,6 @@ class BooksController < ApplicationController
   end
 
   def create
-    
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @user = current_user
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+before_action :authenticate_user!
 before_action :current_user, only: [:edit, :update]
 
   def new
